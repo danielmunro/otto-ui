e15585ef8f8271ec85ad5a70de4377af6e9df1b2
Dan Munro <dan@danmunro.com>
2021-12-31 09:41:35 -0800

    revert this change

--- app/utils/request.js
+++ app/utils/request.js
@@ -38,10 +38,7 @@ function checkStatus(response) {
  * @return {object}           The response data
  */
 export default function request(url, options) {
-  return fetch(
-    `/api/v1/namespaces/default/services/api-gw:8000/proxy/${url}`,
-    options,
-  )
+  return fetch(url, options)
     .then(checkStatus)
     .then(parseJSON);
 }
