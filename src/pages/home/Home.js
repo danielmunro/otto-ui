421f746cbc2f47f6400b8afd6b1072ab312cd557
Dan Munro <dan@danmunro.com>
2022-11-07 07:04:51 -0800

    add homepage welcome message

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,9 @@
+import { Box, Paper, Typography } from '@mui/material';
 import React, {
   useContext,
   useEffect,
 } from 'react';
+import { Link } from 'react-router-dom';
 import { getPosts as requestGetPosts } from '../../actions/post';
 import Container from '../../components/Container';
 import NewPost from './components/NewPost';
@@ -35,6 +37,17 @@ export default function Home() {
 
   return (
     <Container title={"Home"}>
+      { !loggedInUser && (
+        <Paper sx={{p: 1}}>
+          <Typography>
+            Third Place is an open source social network.
+            The <Link to="//thirdplaceapp.com/p/277ad822-c24a-4801-83ad-7cc6a68b4564">code that powers this site</Link> is free for anyone to download, view, and modify. We embrace open source because that is how we learned to program, and we hope others find this project useful in their learning journey.
+          </Typography>
+          <Typography sx={{paddingTop: 1}}>
+            While this site is in closed beta, invite codes are required for signing up.
+          </Typography>
+        </Paper>
+      )}
       { loggedInUser && (
         <NewPost onPostCreated={getPosts} />
       )}
